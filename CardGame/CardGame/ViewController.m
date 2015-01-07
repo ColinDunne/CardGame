5b42522ade7986fe267f3dad055f3e50e99f538a
QianChen <xgdjqc@126.com>
2015-01-07 20:48:56 +0800

    flipping through the deck

--- CardGame/CardGame/ViewController.m
+++ CardGame/CardGame/ViewController.m
@@ -7,28 +7,42 @@
 //
 
 #import "ViewController.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipLabel;
 @property (nonatomic) NSUInteger flipCounts;
+@property (strong,nonatomic) PlayingCardDeck *cardDeck;
 @end
 
 @implementation ViewController
 
 - (IBAction)touchCardButton:(UIButton *)sender {
-    if ([sender.currentTitle isEqualToString:@""]) {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
-        [sender setTitle:@"A♠︎" forState:UIControlStateNormal];
-    } else {
+    if ([sender.currentTitle length]) {
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
+        self.flipCounts++;
+    } else {
+        PlayingCard *card = (PlayingCard *)[self.cardDeck drawRandomCard];
+        if (card) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
+            [sender setTitle:[card contents] forState:UIControlStateNormal];
+            self.flipCounts++;
+        }
     }
-    self.flipCounts++;
 }
 
 - (void)setFlipCounts:(NSUInteger)flipCounts {
     _flipCounts = flipCounts;
     self.flipLabel.text = [NSString stringWithFormat:@"FlipCounts:%lu",(unsigned long)self.flipCounts];
+}
+
+- (PlayingCardDeck *)cardDeck {
+    if (!_cardDeck) {
+        _cardDeck = [[PlayingCardDeck alloc] init];
+    }
+    return _cardDeck;
 }
 
 
